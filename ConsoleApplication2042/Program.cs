dd1812f275eede6385cb1726d63fb41275d4c2ca
ja <ja@JA-KOMPUTER>
2016-11-20 20:46:56 +0100

    test

---
+++ ConsoleApplication2042/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2042
+        }
+    }
+}
